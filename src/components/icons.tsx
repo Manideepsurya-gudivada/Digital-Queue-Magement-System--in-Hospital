8c2d2e4966a3144c035c6ec9cb6bf0672185a52b
Manideep Surya Gudivada <gudivadamanideepsurya@gmail.com>
2025-10-22 16:03:46 +0000

    replace the logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,15 +5,11 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      fill="currentColor"
       {...props}
     >
-      <path d="M12 6v6l4 2" />
-      <path d="M12 2a10 10 0 1 0 10 10" />
+        <path d="M20 8l-8-7-8 7v11a2 2 0 002 2h12a2 2 0 002-2V8z" fill="#fff" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" />
+        <path d="M13 16h-2v-3H8v-2h3V8h2v3h3v2h-3v3z" fill="currentColor" stroke="none" />
     </svg>
   );
 }
